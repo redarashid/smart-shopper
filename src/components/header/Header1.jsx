6641f965cdce0d60eb1562868abe95c3cf66840a
redarashid <redarashid824@gmail.com>
2024-11-02 01:05:43 +0200

    first commit

--- src/components/header/Header1.jsx
+++ src/components/header/Header1.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { ColorModeContext } from "../../theme";
-import { IconButton, useTheme } from "@mui/material";
+import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
 import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
 
 const Header1 = () => {
@@ -14,20 +14,41 @@ const Header1 = () => {
   };
 
   return (
-    <div>
-      <div>
-        <IconButton
-          onClick={handleToggle}
-          color="inherit"
-          aria-label="toggle theme">
-          {theme.palette.mode === "light" ? (
-            <LightModeOutlined />
-          ) : (
-            <DarkModeOutlined />
-          )}
-        </IconButton>
-      </div>
-    </div>
+    <Box sx={{ bgcolor: "#2B3445" }}>
+      <Stack direction={"row"} alignItems={"center"} display={"flex"}>
+        <Typography
+          variant="body2"
+          sx={{
+            bgcolor: "#1F2937",
+            p: "3px 10px",
+            mr: 2,
+            color: "#FFF",
+            fontWeight: "600",
+            fontSize: "12px",
+            borderRadius: 12,
+          }}>
+          Hot
+        </Typography>
+        <Typography variant="body2" sx={{ color: "#FFF", fontSize: "12px" }}>
+          Free Express Shipping
+        </Typography>
+
+        <Box flexGrow={1} />
+
+        <div>
+          <IconButton
+            onClick={handleToggle}
+            color="inherit"
+            aria-label="toggle theme">
+            {theme.palette.mode === "light" ? (
+              <LightModeOutlined />
+            ) : (
+              <DarkModeOutlined />
+            )}
+          </IconButton>
+        </div>
+      </Stack>
+    </Box>
   );
 };
 
