23b6a657daf67bee10109c8f0eee00a13637f63d
redarashid <redarashid824@gmail.com>
2024-11-02 00:43:26 +0200

    first commit

---
+++ src/components/header/Header1.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import { IconButton, useTheme } from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+
+const Header1 = () => {
+  const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
+
+  const handleToggle = () => {
+    const newMode = theme.palette.mode === "dark" ? "light" : "dark";
+    localStorage.setItem("mode", newMode);
+    colorMode.toggleColorMode();
+  };
+
+  return (
+    <div>
+      <div>
+        <IconButton onClick={handleToggle} color="inherit" aria-label="toggle theme">
+          {theme.palette.mode === "light" ? <LightModeOutlined /> : <DarkModeOutlined />}
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Header1;
---
+++ src/theme.jsx
@@ -0,0 +1,59 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+import { grey } from "@mui/material/colors";
+
+export const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    ...(mode === "light"
+      ? {
+          // palette values for light mode
+          text: {
+            primary: "#2B3445",
+          },
+          neutral: {
+            main: "#64748B",
+          },
+          favColor: {
+            main: grey[300],
+          },
+        }
+      : {
+          // palette values for dark mode
+          neutral: {
+            main: "#64748B",
+          },
+          favColor: {
+            main: grey[800],
+          },
+          text: {
+            primary: "#fff",
+          },
+        }),
+  },
+});
+
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState(() => localStorage.getItem("mode") || "light");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prev) => {
+          const newMode = prev === "light" ? "dark" : "light";
+          localStorage.setItem("mode", newMode);
+          return newMode;
+        });
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  return [theme, colorMode];
+};
